57db5888be5f2ee65538961eb5ae6726588fd5ce
t1thomas <timmy.thomas@students.plymouth.ac.uk>
2020-02-04 04:47:48 +0000

    Created ability to send temp code to server

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import GitAuth from '../views/GitAuthentication.vue';
 
 Vue.use(VueRouter);
 
@@ -10,13 +11,18 @@ const routes = [
     name: 'home',
     component: Home,
   },
+  // {
+  //   path: '/gitauth/callback',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/gitauth/callback',
+    name: 'GitAuth',
+    component: GitAuth,
   },
 ];
 
