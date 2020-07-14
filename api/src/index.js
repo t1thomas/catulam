99325381dd96d1ce6d2db605133a828e9483327d
t1thomas <v.tim.thomas@gmail.com>
2020-07-14 11:13:13 +0100

    testing variable change aws 3

--- api/src/index.js
+++ api/src/index.js
@@ -51,7 +51,7 @@ server.applyMiddleware({ app, cors: corsOptions });
 
 const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer);
-
+console.log(corsOptions);
 httpServer.listen(PORT, () => {
   console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
   console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`);
