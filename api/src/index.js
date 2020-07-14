d7536daee3da81436472e7f98bd867ff46b7f3e4
t1thomas <v.tim.thomas@gmail.com>
2020-07-14 13:57:07 +0100

    change

--- api/src/index.js
+++ api/src/index.js
@@ -53,7 +53,7 @@ const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer);
 
 httpServer.listen(PORT, () => {
-  console.log('12:26');
+  console.log('latest deployment');
   console.log(corsOptions);
   console.log(process.env.GRAPHQL_LISTEN_PORT);
   console.log(process.env.CORS_ORIGIN);
