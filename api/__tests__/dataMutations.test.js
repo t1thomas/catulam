a0b84427848526ee92e3e5ff67599807a3d7d04a
t1thomas <v.tim.thomas@gmail.com>
2020-08-25 03:49:34 +0100

    updated docker build config

--- api/__tests__/dataMutations.test.js
+++ api/__tests__/dataMutations.test.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const setCookie = require('set-cookie-parser');
 const initDb = require('../src/seed/seedDb');
 const clearDb = require('../src/clearDb/clearDb');
+const { startServer, stopServer } = require('../test-resourses/testserver');
 const authScopes = require('../src/authScopes');
 
 const gqlQueries = require('../test-resourses/gql-queries-mutations');
@@ -14,11 +15,13 @@ require('dotenv').config();
 const httpEndpoint = process.env.GRAPHQL_URI;
 beforeAll(async () => {
   await initDb();
+  await startServer();
 });
 // clear all db data after tests finish
 
 afterAll(async () => {
   await clearDb();
+  await stopServer();
 });
 
 describe('Login as PM', () => {
